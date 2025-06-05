75ec2eaeefd1fe6616c66970d509bedb14c2a0f4
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-06-05 20:52:50 +0330

    feat: add common layer
    add common layer in order to handle some cross-cutting concerns, shared
    logics, configurations and so on

--- src/API/Peyghom.Api/Program.cs
+++ src/API/Peyghom.Api/Program.cs
@@ -1,4 +1,10 @@
+using Peyghom.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 builder.Services.AddOpenApi("peyghom");
 
