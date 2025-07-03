45c6e56fe206f996a5dec0b6b248d69b5b8c6c59
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-07-03 21:25:38 +0330

    feat: complete common moule

---
+++ src/Common/Peyghom.Common/Presentation/Endpoints/EndpointExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Peyghom.Common.Presentation.Endpoints;
+
+public static class EndpointExtensions
+{
+    /// <summary>
+    /// This method scans the provided assemblies for classes that implement the IEndpoint 
+    /// interface and registers them as transient services in the DI container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="assemblies"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        // we'll search for the classes that implemented I Endpoint and
+        // register them as transient
+        ServiceDescriptor[] serviceDescriptors = assemblies
+            .SelectMany(a => a.GetTypes())
+            .Where(type => type is { IsAbstract: false, IsInterface: false } &&
+                           type.IsAssignableTo(typeof(IEndpoint)))
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+            .ToArray();
+
+        // ensures that duplicate registrations are avoided
+        services.TryAddEnumerable(serviceDescriptors);
+
+        return services;
+    }
+
+    /// <summary>
+    /// This method retrieves all registered IEndpoint implementations from the DI container 
+    /// and maps them to the application's routing system.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="routeGroupBuilder"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder MapEndpoints(
+        this WebApplication app,
+        RouteGroupBuilder? routeGroupBuilder = null)
+    {
+        // Retrieves all registered implementations of IEndpoint from the DI container
+        IEnumerable<IEndpoint> endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
+
+        foreach (IEndpoint endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(builder);
+        }
+
+        return app;
+    }
+}
